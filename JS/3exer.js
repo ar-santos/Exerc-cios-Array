74ab0e248f6d4e7ddeaf472dc28a35e010929bc8
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-03-17 22:01:34 -0300

    Terceiro exer.

---
+++ JS/3exer.js
@@ -0,0 +1,11 @@
+const valores = [0,1,2,5,6,7,10,9,3,4,15,40]
+
+var total = 0
+
+const valoresSomar = valores.map(Valor => { 
+    if(valores) {
+          return total += Valor
+    }
+})
+
+export default valoresSomar
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import listaDePalavras from './JS/1exer.js'
 import arrayPalavras from './JS/2exer.js'
+import somarValores from './JS/3exer.js'
 
 console.log(listaDePalavras)
 console.log(arrayPalavras)
+console.log(somarValores)
